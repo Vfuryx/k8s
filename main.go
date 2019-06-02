19b35bbbc43e6ac9de1a32ca1647439e064f7b8e
Vfuryx <935661686@qq.com>
2019-06-02 18:14:54 +0800

    add k8s test

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "ok",
+		})
+	})
+
+	r.Run(":8080")
+
+}
